925cad85d040c9bd2fd14b536f8ed41ccf9f89fb
Patryk <sucheckipatryk@gmail.com>
2023-05-05 13:31:08 -0400

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(array) {
+    //My answer
+    const win = array.find(game => game.result === "W");
+    if (win) {
+        return win.year;
+    }
+
+    //Answer from google assistance 
+    // const win = array.find(function(game) {
+    //     return game.result === "W";
+    // });
+    // return win ? win.year : undefined;
+}
